f07568bd215153ba63e71a296badcff2b16cee3a
Carlos <carlos.lima@udla.edu.ec>
2026-04-28 21:06:04 -0500

    resta

--- .vscode/cabecera/main.c
+++ .vscode/cabecera/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "sumar.h"
+#include "resta.h"
 
 int main()
 {
     int a,b;
     printf("ingrese 1er numero: ");
     scanf("%d", &a);
-    printf("ingrese 2donumero: ");
+    printf("ingrese 2do numero: ");
     scanf("%d", &b);
-    printf ("la suma es: %d", sumar(a,b));
-    
+    printf ("\nla suma es: %d\n", sumar(a,b));
+    printf("La resta es: %d",resta(a,b));   
     return 0;
 }
