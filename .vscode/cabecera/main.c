d6cd482fb3fbb40236f4983e4a4f94727d7ef478
Carlos <carlos.lima@udla.edu.ec>
2026-04-28 21:30:33 -0500

    multiplicacion

--- .vscode/cabecera/main.c
+++ .vscode/cabecera/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "sumar.h"
 #include "resta.h"
+#include "multiplicacion.h"
 
 int main()
 {
@@ -10,6 +11,8 @@ int main()
     printf("ingrese 2do numero: ");
     scanf("%d", &b);
     printf ("\nla suma es: %d\n", sumar(a,b));
-    printf("La resta es: %d",resta(a,b));   
+    printf("La resta es: %d",resta(a,b));
+    printf ("\nla suma es: %d\n", multiplicacion(a,b));
+
     return 0;
 }
